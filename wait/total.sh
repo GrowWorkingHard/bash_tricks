d9a8e74cece0eb8e775d92e9f4b2803bf89953c5
francescoS <francesco.serafin.3@gmail.com>
2014-12-13 08:40:56 +0100

    Added headings

--- wait/total.sh
+++ wait/total.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+#************************************************************************************#
+#*          - GrowWorkingHard'script - www.growworkinghard.wordpress.com -          *#
+#************************************************************************************#
+# This file is a script writed to complete our blog's articles (all the external     #
+# reference can be found there). It's needed to explain better concepts and give     #
+# to the users an example or a reference.                                            #
+# We don't give you the warranty that is the best solution, but we do all the        #
+# possible to hit it.                                                                #
+#                                                                                    #
+# You're invited to try our solution and communicate if something does't work or if  #
+# there is a better way to do it!                                                    #
+#                                                                                    #
+# Follow our blog: <http://www.growworkinghard.wordpress.com>                        #
+#                                                                                    #
+# Author: Francesco Serafin                                                          #
+# Date: 2014-12-13                                                                   #
+#************************************************************************************#
 
 ~/.script_bash/small_loop.sh &
 ~/.script_bash/big_loop.sh &
